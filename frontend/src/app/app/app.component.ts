b26629f918084b4c425819c23a9ac2163c6d61cf
Kostya <kostikbushmelev@gmail.com>
2024-11-23 13:22:22 +0300

    сделал keycloak авторизацию

--- frontend/src/app/app/app.component.ts
+++ frontend/src/app/app/app.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {keycloakConfig} from '../keycloak.config';
+import {KeycloakService, KeycloakAngularModule} from 'keycloak-angular';
 
 @Component({
   selector: 'app-app',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    KeycloakAngularModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [KeycloakService]
 })
 export class AppComponent {
+  constructor(private keycloakService: KeycloakService) {
+    this.initKeycloak();
+  }
 
+  private initKeycloak() {
+    try {
+      this.keycloakService.init({
+        config: keycloakConfig,
+        initOptions: {
+          onLoad: 'login-required',
+          checkLoginIframe: false,
+          redirectUri: 'http://localhost:4200/home',
+        },
+        loadUserProfileAtStartUp: true
+      });
+    } catch (error) {
+      console.error("Error during Keycloak initialization", error);
+    }
+  }
 }
