3e055098de5b7835d5a4e9e8e2e9d19540765118
mitrian <onlyvideo01@gmail.com>
2024-11-30 22:35:59 +0300

    refactor: repository annotation

--- src/main/java/itmo/coursework/model/repository/FavouriteGamesRepository.java
+++ src/main/java/itmo/coursework/model/repository/FavouriteGamesRepository.java
@@ -2,6 +2,8 @@ package itmo.coursework.model.repository;
 
 import itmo.coursework.model.entity.FavouriteGames;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FavouriteGamesRepository extends JpaRepository<FavouriteGames, Long> {
 }
--- src/main/java/itmo/coursework/model/repository/GameGenreRepository.java
+++ src/main/java/itmo/coursework/model/repository/GameGenreRepository.java
@@ -2,6 +2,8 @@ package itmo.coursework.model.repository;
 
 import itmo.coursework.model.entity.GameGenre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameGenreRepository extends JpaRepository<GameGenre, Long> {
 }
