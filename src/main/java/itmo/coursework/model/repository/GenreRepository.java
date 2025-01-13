e0130944b943871857c1d37d6e110574a7002475
mitrian <onlyvideo01@gmail.com>
2025-01-13 09:14:15 +0300

    refactor: genre operation by psql procedure

--- src/main/java/itmo/coursework/model/repository/GenreRepository.java
+++ src/main/java/itmo/coursework/model/repository/GenreRepository.java
@@ -2,8 +2,16 @@ package itmo.coursework.model.repository;
 
 import itmo.coursework.model.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+    @Query(value = "SELECT * FROM insert_genre(:genreName)", nativeQuery = true)
+    Genre insertGenre(String genreName);
+
+    @Query("SELECT g FROM Genre g WHERE g.name = :name")
+    Optional<Genre> findByName(String name);
 }
