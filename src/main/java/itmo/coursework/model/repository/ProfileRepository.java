6e2e4788be106696c05fdeb77b5bd921deeb51bf
mitrian <onlyvideo01@gmail.com>
2025-01-13 09:11:35 +0300

    refactor: profile operation by psql procedure

--- src/main/java/itmo/coursework/model/repository/ProfileRepository.java
+++ src/main/java/itmo/coursework/model/repository/ProfileRepository.java
@@ -2,6 +2,8 @@ package itmo.coursework.model.repository;
 
 import itmo.coursework.model.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findByName(String name);
+    @Query(value = "SELECT * FROM insert_profile(:name, :icon)", nativeQuery = true)
+    Profile insertProfile(@Param("name") String name, @Param("icon") String icon);
 }
